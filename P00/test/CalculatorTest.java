a181a5bcdd32aa1c967049b60aaca18e67149b07
wang bo <wangbo0204@outlook.com>
2022-05-30 14:05:56 +0800

    first push to P00 repository in GitHub

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	int a = 4321;
+	int b = 1234;
+
+	Calculator cal = new Calculator();
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+
+		int actual = cal.add(a, b);
+		int expected = 5555;
+		assertEquals(expected, actual);
+	}
+
+	public void testSub() {
+
+		int actual = cal.sub(a, b);
+		int expected = 3087;
+		assertEquals(expected, actual);
+
+	}
+
+	public void testMultiply() {
+
+		int actual = cal.multiply(a, b);
+		int expected = 5332114;
+		assertEquals(expected, actual);
+	}
+
+	public void testDivide() {
+
+		int actual = cal.divide(a, b);
+		int expected = 3;
+		assertEquals(expected, actual);
+	}
+
+}
